d6256eb0166c9b96bd63a824bf75ef0351b9b0c2
Anoop Sownderraj <asownder95@gmail.com>
2019-01-17 01:11:39 -0800

    Added morgan package for logging

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.post('/webhook', (req, res) => {
   console.log(`This is the request object: ${JSON.stringify(req)}`);

468194d4576821701584248b3d2817d83149a31a
Anoop Sownderraj <asownder95@gmail.com>
2019-01-17 12:14:29 -0800

    Implemented authorizationCode endpoint on oauthServer

--- oauthServer.js
+++ oauthServer.js
@@ -8,4 +8,16 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.get('/authorization', (req, res) => {
+  console.log('Hit the authorization endpoint');
+  const authorizationURI = `https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/authorize?response_type=code&client_id=${process.env.ClientID}&redirect_uri=${process.env.RedirectURI}&scope=mb:vehicle:status:general`;
+  console.log(authorizationURI);
+  res.redirect(authorizationURI);
+});
+
+app.get('/code', (req, res) => {
+  console.log(`Hit the code endpoint, code is ${req.params.code}`);
+  res.send();
+});
+
 app.listen(port, () => console.log(`Oauth server listening on port ${port}`));

06f14c4fbf0848dfacb0c7f957b6cfdb44e0a500
Anoop Sownderraj <asownder95@gmail.com>
2019-01-18 02:17:29 -0800

    Implemented LoginRequestHandler

---
+++ handlers/LoginRequestHandler.js
@@ -0,0 +1,13 @@
+exports.LoginRequestHandler = {
+  canHandle(handlerInput) {
+    const { login } = handlerInput.attributesManager.getRequestAttributes();
+    return !!login;
+  },
+  handle(handlerInput) {
+    const speechText = 'You will need to log in to your Mercedes Benz account before using this skill. Follow the instructions that appear in your Alexa app.';
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withLinkAccountCard()
+      .getResponse();
+  },
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Alexa = require('ask-sdk');
 const { LaunchRequestHandler, ErrorHandler } = require('./handlers/builtInIntents');
+const { LoginRequestHandler } = require('./handlers/LoginRequestHandler');
 const { OauthInterceptor } = require('./handlers/auth');
 
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
+    LoginRequestHandler,
     LaunchRequestHandler,
   )
   .addRequestInterceptors(OauthInterceptor)

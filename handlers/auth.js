f7e6fc5d8360a3e76f26402a0820904e9469d761
Anoop Sownderraj <asownder95@gmail.com>
2019-01-18 11:45:58 -0800

    Debugging request handlers

--- handlers/auth.js
+++ handlers/auth.js
@@ -5,16 +5,25 @@ redisClient.on('connect', () => console.log('Redis connected'));
 redisClient.on('error', err => console.error(`Error from Redis: ${JSON.stringify(err)}`));
 
 exports.OauthInterceptor = {
-  process(handlerInput) {
+  async process(handlerInput) {
     try {
       const attributes = handlerInput.attributesManager.getSessionAttributes();
+      console.log(`Step 1: Attributes - ${JSON.stringify(attributes)}`);
       if (!attributes.accessToken) {
-        const tokens = redisClient.get('userId');
+        const tokens = await new Promise((resolve) => {
+          redisClient.get('userId', (err, res) => {
+            resolve(res);
+          });
+        });
+        console.log(`Step 2: tokens ${tokens}`);
         if (!tokens) {
-          handlerInput.attributesManager.setRequestAttributes({ login: true });
+          const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+          handlerInput.attributesManager.setRequestAttributes(Object.assign(requestAttributes, { login: true }));
+          console.log('Step 3: set request attributes');
         } else {
           const [accessToken, refreshToken] = tokens.split(':');
           handlerInput.attributesManager.setSessionAttributes(Object.assign(attributes, { accessToken, refreshToken }));
+          console.log(`Step 4: set session attributes. This is handlerInput: ${JSON.stringify(handlerInput)}`);
         }
       }
     } catch (err) {

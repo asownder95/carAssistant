a172e6d813752112b55878c7b806e9c57501e410
Anoop Sownderraj <asownder95@gmail.com>
2019-01-18 23:47:03 -0800

    Implemented logic for DoorStatus handler

--- handlers/doorHandlers.js
+++ handlers/doorHandlers.js
@@ -16,7 +16,9 @@ const DoorStatusHandler = {
           authorization: `Bearer ${accessToken}`,
         },
       });
-      // Handle logic with results
+      return handlerInput.responseBuilder
+        .speak(generateDoorStatusResponse(results))
+        .getResponse();
     } catch (err) {
       if (err.statusCode === 401) {
         return handleExpiredAccessToken(handlerInput, DoorStatusHandler);
@@ -25,6 +27,36 @@ const DoorStatusHandler = {
     }
   },
 };
+
+function generateDoorStatusResponse (results) {
+  let response = '';
+  const summary = {
+    open: [],
+    unlocked: [],
+  };
+  Object.keys(results).slice(0, -1).forEach((key) => {
+    if (results[key].value === 'OPEN') {
+      summary.open.push(key.slice(10));
+    } else if (results[key].value === 'UNLOCKED') {
+      summary.unlocked.push(key.slice(14));
+    }
+  });
+  if (summary.open.length === 0) {
+    response += 'All the doors in the vehicle are closed. ';
+  } else if (summary.open.length === 4) {
+    response += 'All the doors in the vehicle are open. ';
+  } else {
+    response += `The ${summary.open.join(', ')} ${summary.open.length === 1 ? 'door is' : 'doors are'} open. `;
+  }
+  if (summary.unlocked.length === 0) {
+    response += 'All of the doors are currently locked.';
+  } else if (summary.unlocked.length === 4) {
+    response += 'All of the doors are curently unlocked.';
+  } else {
+    response += `The ${summary.unlocked.join(', ')} doors are currently unlocked.`;
+  }
+  return response;
+}
 
 module.exports = {
   DoorStatusHandler,

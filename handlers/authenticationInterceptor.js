28e50e2fad91ef1fe65b69c081ade927aa333b57
Anoop Sownderraj <asownder95@gmail.com>
2019-01-29 15:45:06 -0800

    Updated authentication interceptor

--- handlers/authenticationInterceptor.js
+++ handlers/authenticationInterceptor.js
@@ -1,28 +1,24 @@
-const redis = require('redis');
-const { REDISCLOUD_URL } = require('../config');
+const request = require('request-promise');
+const { API_ENDPOINT } = require('../config');
 
 exports.AuthenticationInterceptor = {
   async process(handlerInput) {
     try {
-      const attributes = handlerInput.attributesManager.getSessionAttributes();
-      if (!attributes.accessToken) {
-        const tokens = await new Promise((resolve) => {
-          const redisClient = redis.createClient(REDISCLOUD_URL);
-          redisClient.get('userId', (err, res) => {
-            if (err) {
-              throw err;
-            }
-            redisClient.quit();
-            resolve(res);
-          });
-        });
-        if (!tokens) {
-          const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-          handlerInput.attributesManager.setRequestAttributes(Object.assign(requestAttributes, { login: true }));
-        } else {
-          const [accessToken, refreshToken] = tokens.split(':');
-          handlerInput.attributesManager.setSessionAttributes(Object.assign(attributes, { accessToken, refreshToken }));
-        }
+      const { accessToken } = handlerInput.requestEnvelope.context.System.user;
+      const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+      if (!accessToken) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        handlerInput.attributesManager.setRequestAttributes(Object.assign(requestAttributes, { login: true }));
+      } else if (!sessionAttributes.vehicleId) {
+        const vehicleList = JSON.parse(await request({
+          uri: `${API_ENDPOINT}/vehicles`,
+          method: 'GET',
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+            accept: 'application/json',
+          },
+        }));
+        handlerInput.attributesManager.setSessionAttributes(Object.assign(sessionAttributes, { vehicleId: vehicleList[0].id }));
       }
     } catch (err) {
       throw err;
